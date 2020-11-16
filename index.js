7e432d5806f149ac10a2aef678d5a65c608f48d7
Steve <stephenvincentibanez@gmail.com>
2020-11-16 13:13:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement("h1#victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
